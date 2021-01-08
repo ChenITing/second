22d1ccf7173695193397417aa3142ddcc6b336f4
Chen.IT <71428625+ChenITing@users.noreply.github.com>
2021-01-08 22:27:17 +0800

    0

---
+++ link/jq.js
@@ -0,0 +1,107 @@
+function insc9() {
+    var count9=document.getElementById("count9").innerHTML;
+    document.getElementById("count9").innerHTML=parseInt(count9)+1;
+    }
+    function dec9() {
+        var count9=document.getElementById("count9").innerHTML;
+        if(parseInt(count9)>0){
+            document.getElementById("count9").innerHTML=parseInt(count9)-1;		
+        };
+        
+    }
+
+function insc1() {
+    var count1=document.getElementById("count1").innerHTML;
+    document.getElementById("count1").innerHTML=parseInt(count1)+1;
+    }
+    function dec1() {
+        var count1=document.getElementById("count1").innerHTML;
+        if(parseInt(count1)>0){
+            document.getElementById("count1").innerHTML=parseInt(count1)-1;		
+        };
+            
+    }
+
+function insc2() {
+    var count2=document.getElementById("count2").innerHTML;
+    document.getElementById("count2").innerHTML=parseInt(count2)+1;
+    }
+    function dec2() {
+        var count2=document.getElementById("count2").innerHTML;
+        if(parseInt(count2)>0){
+            document.getElementById("count2").innerHTML=parseInt(count2)-1;		
+        };
+            
+    }
+
+function insc3() {
+    var count3=document.getElementById("count3").innerHTML;
+    document.getElementById("count3").innerHTML=parseInt(count3)+1;
+    }
+    function dec3() {
+        var count3=document.getElementById("count3").innerHTML;
+        if(parseInt(count3)>0){
+            document.getElementById("count3").innerHTML=parseInt(count3)-1;		
+        };
+                
+    }
+
+function insc4() {
+    var count4=document.getElementById("count4").innerHTML;
+    document.getElementById("count4").innerHTML=parseInt(count4)+1;
+    }
+    function dec4() {
+        var count4=document.getElementById("count4").innerHTML;
+        if(parseInt(count4)>0){
+            document.getElementById("count4").innerHTML=parseInt(count4)-1;		
+        };
+                    
+    }  
+
+function insc5() {
+    var count5=document.getElementById("count5").innerHTML;
+    document.getElementById("count5").innerHTML=parseInt(count5)+1;
+    }
+    function dec5() {
+        var count5=document.getElementById("count5").innerHTML;
+        if(parseInt(count5)>0){
+            document.getElementById("count5").innerHTML=parseInt(count5)-1;		
+        };
+            
+    }
+
+function insc6() {
+    var count6=document.getElementById("count6").innerHTML;
+    document.getElementById("count6").innerHTML=parseInt(count6)+1;
+    }
+    function dec6() {
+        var count6=document.getElementById("count6").innerHTML;
+        if(parseInt(count6)>0){
+            document.getElementById("count6").innerHTML=parseInt(count6)-1;		
+        };
+            
+    }
+
+function insc7() {
+    var count7=document.getElementById("count7").innerHTML;
+    document.getElementById("count7").innerHTML=parseInt(count7)+1;
+    }
+    function dec7() {
+        var count7=document.getElementById("count7").innerHTML;
+        if(parseInt(count7)>0){
+            document.getElementById("count7").innerHTML=parseInt(count7)-1;		
+        };
+            
+    }
+
+function insc8() {
+    var count8=document.getElementById("count8").innerHTML;
+    document.getElementById("count8").innerHTML=parseInt(count8)+1;
+    }
+    function dec8() {
+        var count8=document.getElementById("count8").innerHTML;
+        if(parseInt(count8)>0){
+            document.getElementById("count8").innerHTML=parseInt(count8)-1;		
+        };
+            
+    }
